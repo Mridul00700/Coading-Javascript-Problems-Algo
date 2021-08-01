58df04ebcf6d29e252985bf05b039b5f9de524f3
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-08-01 12:29:55 +0530

    Q7) LongestSubstring 1*:)- EfficientCode

--- script.js
+++ script.js
@@ -286,3 +286,26 @@ var lengthOfLongestSubstring = function (s) {
 console.log(lengthOfLongestSubstring("abcabcbb"));
 
 // Efficient method... Sliding window method. 
+
+var lengthOfLongestSubstringEfficient = function (s) {
+    if (!s) return 0;
+
+    let start = 0;
+    let end = 0;
+    let maxLength = 0;
+    let uniqueChar = new Set();
+    while (end < s.length) {
+        if (!uniqueChar.has(s[end])) {
+            uniqueChar.add(s[end]);
+            end++;
+            maxLength = Math.max(maxLength, uniqueChar.size);
+        }
+        else {
+            uniqueChar.delete(s[start]);
+            start++;
+        }
+    }
+    return maxLength;
+};
+
+console.log(lengthOfLongestSubstringEfficient("abcabcbb"));
