1ba4d093cd44b195b38ec13c3cddda08e4abfcde
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-08-10 14:47:56 +0530

    combination-sum 1+2+1 +4 3+1

--- script.js
+++ script.js
@@ -565,3 +565,35 @@ var addStrings = function (num1, num2) {
 };
 
 console.log(addStrings("91", "93"));
+
+/*
+Q16) Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+
+The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+
+It is guaranteed that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+Input: candidates = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+*/
+var findAns = (candidates, start = 0, remainingSum, finalComb, currentComb) => {
+    if (remainingSum < 0) {
+        return finalComb;
+    }
+    if (remainingSum === 0) {
+        finalComb.push(currentComb.slice());
+        return finalComb;
+    }
+    for (let j = start; j < candidates.length; j++) {
+        const current = candidates[j];
+        currentComb.push(current);
+        findAns(candidates, j, remainingSum - current, finalComb, currentComb);
+        currentComb.pop();
+    }
+    return finalComb;
+}
+
+var combinationSum = function (candidates, target) {
+    return findAns(candidates, 0, target, [], []);
+};
+
+console.log(combinationSum([2, 3, 6, 7]), 7);
