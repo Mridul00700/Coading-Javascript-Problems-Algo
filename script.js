0489b6229407f1a005c0c4a9de7aac73d06b9cf9
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-08-11 10:53:00 +0530

    min-flip-to-monotonus-increasing

--- script.js
+++ script.js
@@ -597,3 +597,33 @@ var combinationSum = function (candidates, target) {
 };
 
 console.log(combinationSum([2, 3, 6, 7]), 7);
+
+/*
+Q17) A binary string is monotone increasing if it consists of some number of 0's (possibly none), followed by
+some number of 1's (also possibly none).
+
+You are given a binary string s. You can flip s[i] changing it from 0 to 1 or from 1 to 0.
+
+Return the minimum number of flips to make s monotone increasing.
+*/
+
+var minFlipsMonoIncr = function (s) {
+    let flip = 0;
+    let one = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '0') {
+            if (one === 0)
+                continue;
+            else {
+                flip++;
+            }
+        }
+        else one++;
+
+        if (flip > one)
+            flip = one;
+    }
+    return flip;
+};
+console.log(minFlipsMonoIncr("00110"));
